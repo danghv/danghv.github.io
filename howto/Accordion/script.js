d5d3589d0e5585f72d321b0fd41eab9c7f58739e
Dang <hadangktmt@gmail.com>
2017-08-08 16:13:11 +0700

    08/08/2017

--- howto/Accordion/script.js
+++ howto/Accordion/script.js
@@ -1,13 +1,17 @@
-// var x = document.getElementsByClassName("accordion");
-// 	for(var i = 0; i < x.length; i++){x[i].addEventListener("click", function(){
-// 		this.nextElementSibling.classList.toggle("active");
-// 		this.classList.toggle("active");
-// 	});}
-	// for (var i = 0; i < x.length; i++) {
-	// 	x[i].onclick= function(){
-	// 	this.nextElementSibling.classList.toggle("active")
-	// };
+var x = document.getElementsByClassName("accordion");
+console.log(x);
+	for(var i = 0; i < x.length; i++){
+		x[i].addEventListener("click", function(){
 
+		this.nextElementSibling.classList.toggle("active");
+		this.classList.toggle("active");
+	});}
+// 	for (var i = 0; i < x.length; i++) {
+// 		x[i].onclick= function(){
+// 		this.nextElementSibling.classList.toggle("active")
+// 	}
+
+// }
 	/*
 	this use js core, now let's use jQuery
 	 */
@@ -22,15 +26,15 @@
 // 	}
 // });
 
-var acc = $(".accordion");
-var i;
+// var acc = $(".accordion");
+// var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].click = function(){
-        this.toggleClass("active");
-        this.next().toggleClass('active');
-    }
-}
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].click = function(){
+//         this.toggleClass("active");
+//         this.next().toggleClass('active');
+//     }
+// }
 
 /*
 wtfffffff is this???????????? move on
