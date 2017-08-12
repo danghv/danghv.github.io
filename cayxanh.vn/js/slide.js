5a3bc95727d0882bd2824a5dc76d304930432e35
Dang <hadangktmt@gmail.com>
2017-08-12 15:52:54 +0700

    12/08

---
+++ cayxanh.vn/js/slide.js
@@ -0,0 +1,88 @@
+// process slides
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+	console.log(slideIndex);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName('my-slides');
+	var dots = document.getElementsByClassName('slide-dot');
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n<1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(' active', '');
+	}
+	slides[slideIndex - 1].style.display = 'block';
+	dots[slideIndex - 1].className += ' active';
+
+	console.log(slides[slideIndex - 1].className);
+}
+
+//process tab new content Hot Trends
+
+function openContent(evt, tabId) {
+	var i, tablinks, tabcontents;
+	tablinks = document.getElementsByClassName('tablinks');
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(' active', '');
+	}
+	tabcontents = document.getElementsByClassName('tabcontent');
+	for (var i = 0; i < tabcontents.length; i++) {
+		tabcontents[i].style.display = 'none';
+	}
+	document.getElementById(tabId).style.display = 'block';
+	evt.currentTarget.className += ' active';
+}
+
+document.getElementById('openDefault').click();
+
+//process Cay xanh vuon nha
+function openContentGarden(evt, tabId) {
+	var i, tablinksGarden, tabcontentsGarden;
+	tablinksGarden = document.getElementsByClassName('tablinks-garden');
+	for (i = 0; i < tablinksGarden.length; i++) {
+		tablinksGarden[i].className = tablinksGarden[i].className.replace(' active', '');
+	}
+	tabcontentsGarden = document.getElementsByClassName('tabcontent-garden');
+	for (var i = 0; i < tabcontentsGarden.length; i++) {
+		tabcontentsGarden[i].style.display = 'none';
+	}
+	document.getElementById(tabId).style.display = 'block';
+	evt.currentTarget.className += ' active';
+}
+
+document.getElementById('openDefaultGarden').click();
+
+//process sản phẩm Hỗ Trợ
+
+function openContentSupport(evt, tabId) {
+	var i, tablinksSupport, tabcontentsSupport;
+	tablinksSupport = document.getElementsByClassName('tablinks-support');
+	for (i = 0; i < tablinksSupport.length; i++) {
+		tablinksSupport[i].className = tablinksSupport[i].className.replace(' active', '');
+	}
+	tabcontentsSupport = document.getElementsByClassName('tabcontent-support');
+	for (var i = 0; i < tabcontentsSupport.length; i++) {
+		tabcontentsSupport[i].style.display = 'none';
+	}
+	document.getElementById(tabId).style.display = 'block';
+	evt.currentTarget.className += ' active';
+}
+
+document.getElementById('openDefaultSupport').click();
