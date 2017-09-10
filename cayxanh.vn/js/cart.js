80793676618e4282d4e9251074071e68d00b6124
Dang <hadangktmt@gmail.com>
2017-09-10 18:09:55 +0700

    10/09

---
+++ cayxanh.vn/js/cart.js
@@ -0,0 +1,11 @@
+var dongiarow1 = document.getElementById('dongiarow1').innerHTML;
+
+var dongiarow2 = document.getElementById('dongiarow2').innerHTML;
+
+var soluongrow1 = document.getElementById('soluongrow1').innerHTML;
+
+var soluongrow2 = document.getElementById('soluongrow2').innerHTML;
+
+function plus(){
+	soluongrow1 += 1;
+}
