a7939250de672ae867a3b16d3da11e1338e876c7
Dang <hadangktmt@gmail.com>
2017-09-11 21:03:05 +0700

    11/09

--- cayxanh.vn/js/cart.js
+++ cayxanh.vn/js/cart.js
@@ -1,11 +1,12 @@
-var dongiarow1 = document.getElementById('dongiarow1').innerHTML;
 
-var dongiarow2 = document.getElementById('dongiarow2').innerHTML;
 
-var soluongrow1 = document.getElementById('soluongrow1').innerHTML;
-
-var soluongrow2 = document.getElementById('soluongrow2').innerHTML;
-
-function plus(){
-	soluongrow1 += 1;
+function plus(number, numberId, rowId){
+	var dongia = document.getElementById('dongiarow1').innerHTML;
+	var dongiaConvert = dongia.substring(0, dongia.length - 4).replace(/\./g, "");
+	var quantity = Number(document.getElementById(numberId).innerHTML);
+	var rowtotalStr = Number(dongiaConvert) * (quantity + number);
+	document.getElementById(numberId).innerHTML = quantity + number;
+	document.getElementById(rowId).innerHTML = rowtotalStr.toLocaleString('vi') + ' vnđ';
+	
 }
+
